86cca0abf8aabf1f9b290d9e51acd32ad9954123
SSSSSADO <grushkovik.danil@gmail.com>
2025-11-12 23:48:32 +0300

    fix

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-print("Hello World")
-# test
+class Book:
+    def __init__(self, name: str, author: str):
+        self.name = name
+        self.author = author
+
+book = Book("My favourite book", 42)
+print(1)
+print(book.name)
+print(book.author)
